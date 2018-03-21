5b855771335c3211ea7531e47ce1e29fc1d759c2
jmcacioppo <jmcacioppo0531@gmail.com>
2018-03-21 10:46:20 -0400

    Showing different menus on week view, got rid of #! in url, centered header

--- js/controllers/WeekMenuController.js
+++ js/controllers/WeekMenuController.js
@@ -1,5 +1,75 @@
-gatorEats.controller('WeekMenuController', ['$scope',
-    function($scope) {
+gatorEats.controller('WeekMenuController', ['$scope', '$http',
+    function($scope, $http) {
+    	$scope.freshFoodBreakfast = true;
+    	$scope.freshFoodLunch = false;
+    	$scope.freshFoodDinner = false;
+    	$scope.gatorCornerBreakfast = false;
+    	$scope.gatorCornerLunch = false;
+    	$scope.gatorCornerDinner = false;
 
+    	$scope.showMenu = function(menu) {
+    		console.log(menu);
+    		if(menu == 'FF Breakfast') showFFBreakfast();
+    		else if(menu == 'FF Lunch') showFFLunch();
+    		else if(menu == 'FF Dinner') showFFDinner();
+    		else if(menu == 'GC Breakfast') showGCBreakfast();
+    		else if(menu == 'GC Lunch') showGCLunch();
+    		else if(menu == 'GC Dinner') showGCDinner();
+    	}
+
+
+    	function showFFBreakfast() {
+    		$scope.freshFoodBreakfast = true;
+	    	$scope.freshFoodLunch = false;
+	    	$scope.freshFoodDinner = false;
+	    	$scope.gatorCornerBreakfast = false;
+	    	$scope.gatorCornerLunch = false;
+	    	$scope.gatorCornerDinner = false;
+    	}
+
+    	function showFFLunch() {
+    		$scope.freshFoodBreakfast = false;
+	    	$scope.freshFoodLunch = true;
+	    	$scope.freshFoodDinner = false;
+	    	$scope.gatorCornerBreakfast = false;
+	    	$scope.gatorCornerLunch = false;
+	    	$scope.gatorCornerDinner = false;
+    	}
+
+    	function showFFDinner() {
+    		$scope.freshFoodBreakfast = false;
+	    	$scope.freshFoodLunch = false;
+	    	$scope.freshFoodDinner = true;
+	    	$scope.gatorCornerBreakfast = false;
+	    	$scope.gatorCornerLunch = false;
+	    	$scope.gatorCornerDinner = false;
+    	}
+
+    	function showGCBreakfast() {
+    		$scope.freshFoodBreakfast = false;
+	    	$scope.freshFoodLunch = false;
+	    	$scope.freshFoodDinner = false;
+	    	$scope.gatorCornerBreakfast = true;
+	    	$scope.gatorCornerLunch = false;
+	    	$scope.gatorCornerDinner = false;
+    	}
+
+    	function showGCLunch() {
+    		$scope.freshFoodBreakfast = false;
+	    	$scope.freshFoodLunch = false;
+	    	$scope.freshFoodDinner = false;
+	    	$scope.gatorCornerBreakfast = false;
+	    	$scope.gatorCornerLunch = true;
+	    	$scope.gatorCornerDinner = false;
+    	}
+
+    	function showGCDinner() {
+    		$scope.freshFoodBreakfast = false;
+	    	$scope.freshFoodLunch = false;
+	    	$scope.freshFoodDinner = false;
+	    	$scope.gatorCornerBreakfast = false;
+	    	$scope.gatorCornerLunch = false;
+	    	$scope.gatorCornerDinner = true;
+    	}
     }
 ]);
