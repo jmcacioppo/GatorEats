ad93c9f9197ba1573f5727c3ecc2cb91429e40b9
jmcacioppo <jmcacioppo0531@gmail.com>
2018-03-23 17:19:22 -0400

    All jsons load in week menu controller. Display on page and preliminary view is setup with no styling yet

--- js/controllers/WeekMenuController.js
+++ js/controllers/WeekMenuController.js
@@ -7,8 +7,9 @@ gatorEats.controller('WeekMenuController', ['$scope', '$http',
     	$scope.gatorCornerLunch = false;
     	$scope.gatorCornerDinner = false;
 
+    	getMenus();
+
     	$scope.showMenu = function(menu) {
-    		console.log(menu);
     		if(menu == 'FF Breakfast') showFFBreakfast();
     		else if(menu == 'FF Lunch') showFFLunch();
     		else if(menu == 'FF Dinner') showFFDinner();
@@ -16,7 +17,6 @@ gatorEats.controller('WeekMenuController', ['$scope', '$http',
     		else if(menu == 'GC Lunch') showGCLunch();
     		else if(menu == 'GC Dinner') showGCDinner();
     	}
-
 
     	function showFFBreakfast() {
     		$scope.freshFoodBreakfast = true;
@@ -70,6 +70,58 @@ gatorEats.controller('WeekMenuController', ['$scope', '$http',
 	    	$scope.gatorCornerBreakfast = false;
 	    	$scope.gatorCornerLunch = false;
 	    	$scope.gatorCornerDinner = true;
+    	}
+
+    	function getMenus() {
+    		// Fresh Food Menus
+    		$http.get('menus/FFbreakfast.json')
+	    		.then( (res) => {
+	    			$scope.FFBreakfast = res.data;
+	    		})
+	    		.catch( (err) =>{
+	    			console.log(err);
+	    		});
+
+	    	$http.get('menus/FFlunch.json')
+	    		.then( (res) => {
+	    			$scope.FFLunch = res.data;
+	    		})
+	    		.catch( (err) =>{
+	    			console.log(err);
+	    		});
+
+	    	$http.get('menus/FFdinner.json')
+	    		.then( (res) => {
+	    			$scope.FFDinner = res.data;
+	    		})
+	    		.catch( (err) =>{
+	    			console.log(err);
+	    		});
+
+	    	// Gator Corner Menus
+	    	$http.get('menus/GCbreakfast.json')
+	    		.then( (res) => {
+	    			$scope.GCBreakfast = res.data;
+	    		})
+	    		.catch( (err) =>{
+	    			console.log(err);
+	    		});
+
+	    	$http.get('menus/GClunch.json')
+	    		.then( (res) => {
+	    			$scope.GCLunch = res.data;
+	    		})
+	    		.catch( (err) =>{
+	    			console.log(err);
+	    		});
+
+	    	$http.get('menus/GCdinner.json')
+	    		.then( (res) => {
+	    			$scope.GCDinner = res.data;
+	    		})
+	    		.catch( (err) =>{
+	    			console.log(err);
+	    		});
     	}
     }
 ]);
