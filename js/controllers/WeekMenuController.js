58e74f9d618c2269d871971dc3dc469db6972036
jmcacioppo <jmcacioppo0531@gmail.com>
2018-03-25 22:24:27 -0400

    Displaying all data in jsons, need to style still

--- js/controllers/WeekMenuController.js
+++ js/controllers/WeekMenuController.js
@@ -92,7 +92,7 @@ gatorEats.controller('WeekMenuController', ['$scope', '$http',
 
 	    	$http.get('menus/FFdinner.json')
 	    		.then( (res) => {
-	    			$scope.FFDinner = res.data;
+	    			$scope.FFDinner = res.data.FFdinner;
 	    		})
 	    		.catch( (err) =>{
 	    			console.log(err);
